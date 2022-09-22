a8b0adf545bff7c5543281316fefcb92aa42826f
Oscar Dominguez <dominguez.celada@gmail.com>
2022-09-22 13:53:31 +0200

    feat: Initial version (#1)
    * docs(README): remove WIP note
    
    * build(package): initial version
    
    * build(package): lock file
    
    * build(gitignore): node_modules
    
    * feat: initial version
    
    * docs(README): badges
    
    * docs(README): usage
    
    * ci(release): initial version
    
    * ci(test): initial version
    
    * feat(script): implement logic to modify GitHub workflows' Action version
    
    * docs(readme): add 'Caveats' section alerting users will lose comments in .yaml files if modified

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-# 🚧 WORK IN PROGRESS. See [#1](https://github.com/oscard0m/octoherd-script-update-action-version-in-workflows/pull/1) | [Preview](https://github.com/oscard0m/octoherd-script-update-action-version-in-workflows/tree/initial-version)
-
 # octoherd-script-update-action-version-in-workflows
 
 > Update workflows using a certain GitHub Action to a concrete version, i.e. actions/checkout@v{version_number}
+
+[![@latest](https://img.shields.io/npm/v/octoherd-script-update-action-version-in-workflows.svg)](https://www.npmjs.com/package/octoherd-script-update-action-version-in-workflows)
+[![Build Status](https://github.com/oscard0m/octoherd-script-update-action-version-in-workflows/workflows/Test/badge.svg)](https://github.com/oscard0m/octoherd-script-update-action-version-in-workflows/actions?query=workflow%3ATest+branch%3Amain)
+
+## Usage
+
+Minimal usage
+
+```js
+npx octoherd-script-update-action-version-in-workflows \
+        --actionName actions/checkout \
+        --actionVersion v3
+```
+
+Pass all options as CLI flags to avoid user prompts
+
+```js
+npx octoherd-script-update-action-version-in-workflows \
+  -T ghp_0123456789abcdefghjklmnopqrstuvwxyzA \
+  -R "oscard0m/*" \
+        --actionName actions/checkout \
+        --actionVersion v3 \
+        --workflow my-workflow.yml
+```
+
+## Options
+
+| option                       | type             | description                                                                                                                                                                                                                                 |
+| ---------------------------- | ---------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `--action-name`              | string           | **Required.** Name of the GitHub Action you want to update its version in your workflows                                                                                                                                                    |
+| `--action-version`           | string           | **Required.** Version you want to change of the selected GitHub Action                                                                                                                                                                      |
+| `--workflow`                 | string           | Name of the workflow you want to apply the changes. Defaults to "\*"                                                                                                                                                                        |
+| `--octoherd-token`, `-T`     | string           | A personal access token ([create](https://github.com/settings/tokens/new?scopes=repo)). Script will create one if option is not set                                                                                                         |
+| `--octoherd-repos`, `-R`     | array of strings | One or multiple space-separated repositories in the form of `repo-owner/repo-name`. `repo-owner/*` will find all repositories for one owner. `*` will find all repositories the user has access to. Will prompt for repositories if not set |
+| `--octoherd-bypass-confirms` | boolean          | Bypass prompts to confirm mutating requests                                                                                                                                                                                                 |
+
+## Caveats
+
+`js-yaml` does not preserve comments in yaml files so bear in mind this script might get rid of the comments inside the modified files:
+
+- https://github.com/nodeca/js-yaml/issues/624
+- https://github.com/nodeca/js-yaml/issues/494
+- https://github.com/nodeca/js-yaml/issues/305
+- https://github.com/nodeca/js-yaml/issues/196
 
 ## Contributing
 
---
+++ script.js
@@ -0,0 +1,166 @@
+// @ts-check
+
+import { isMatch } from "matcher";
+import { composeCreateOrUpdateTextFile } from "@octokit/plugin-create-or-update-text-file";
+import yaml from "js-yaml";
+import chalk from "chalk";
+import terminalLink from "terminal-link";
+
+/**
+ * Update workflows using a certain GitHub Action to a concrete version, i.e. actions/checkout@v{version_number}
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} options.actionName Name of the GitHub Action you want to update its version in your workflows
+ * @param {string} options.actionVersion Version you want to change of the selected GitHub Action
+ * @param {string} [options.workflow] Name of the workflow you want to apply the changes
+ */
+export async function script(
+  octokit,
+  repository,
+  { actionName, actionVersion, workflow = "*" }
+) {
+  if (!actionName) {
+    throw new Error(chalk.red("✖ --action-name is required"));
+  }
+
+  if (!actionVersion) {
+    throw new Error(chalk.red("✖ --action-version is required"));
+  }
+
+  if (repository.archived) {
+    octokit.log.info(chalk.gray("▶▶ Repository is archived, ignoring"));
+    return;
+  }
+
+  const owner = repository.owner.login;
+  const repo = repository.name;
+
+  // find all workflow files
+  const { data: files } = await octokit
+    .request("GET /repos/{owner}/{repo}/contents/{path}", {
+      owner,
+      repo,
+      path: ".github/workflows",
+    })
+    .catch((error) => {
+      if (error.status !== 404) throw error;
+
+      // if `.github/workflows` does not exist we treat it an empty directory
+      return { data: [] };
+    });
+
+  if (!Array.isArray(files)) {
+    octokit.log.warn(
+      chalk.yellow(`⚠ .github/workflows is not a directory. Ignoring`)
+    );
+    return;
+  }
+
+  if (files.length === 0) {
+    octokit.log.info(chalk.gray(`◯ No workflow files to update`));
+    return;
+  }
+
+  const fileNames = files.map((file) => file.name);
+  const ignored = [];
+  const filteredFileNames = fileNames.filter((name) => {
+    if (isMatch(name, workflow)) return true;
+    ignored.push(name);
+  });
+
+  if (ignored.length) {
+    octokit.log.debug(
+      chalk.gray(`▶▶ Ignored workflows: ${ignored.join(", ")}`)
+    );
+  }
+
+  for (const fileName of filteredFileNames) {
+    try {
+      const {
+        updated,
+        // @ts-ignore - incorrect types returned by `composeCreateOrUpdateTextFile`
+        data: { commit },
+      } = await composeCreateOrUpdateTextFile(octokit, {
+        owner,
+        repo,
+        path: `.github/workflows/${fileName}`,
+        content({ content }) {
+          /** @type {import("./types").Workflow} */
+          let config;
+          let actionVersionChanged = false;
+
+          if (!content) {
+            return content;
+          }
+
+          try {
+            config = yaml.load(content);
+          } catch (error) {
+            octokit.log.warn(chalk.yellow(`  ⚠ Invalid YAML: ${fileName}`));
+            return content;
+          }
+
+          if (!config?.jobs) {
+            octokit.log.warn(chalk.yellow(`  ⚠ No jobs in ${fileName}`));
+            return content;
+          }
+
+          for (const [jobName, job] of Object.entries(config.jobs)) {
+            if (!job.steps) {
+              octokit.log.warn(
+                chalk.yellow(`  ⚠ No steps in ${fileName} -> [job]: ${jobName}`)
+              );
+              continue;
+            }
+
+            for (const step of job.steps) {
+              if ("uses" in step && step.uses.startsWith(actionName)) {
+                if (step.uses === `${actionName}@${actionVersion}`) {
+                  octokit.log.info(
+                    chalk.gray(
+                      ` ▶▶ ${fileName} -> [job]: ${jobName} -> already uses ${actionName}@${actionVersion}`
+                    )
+                  );
+                } else {
+                  step.uses = `${actionName}@${actionVersion}`;
+                  actionVersionChanged = true;
+                }
+              }
+            }
+          }
+
+          if (!actionVersionChanged) return content;
+
+          return yaml.dump(config, {
+            quotingType: '"',
+          });
+        },
+        message: `ci(${fileName}): set version ${actionVersion} to ${actionName}`,
+      });
+
+      if (updated) {
+        octokit.log.info(
+          chalk.green(
+            `  ✔ '${fileName}' updated ${terminalLink(
+              "(Link to commit)",
+              commit.html_url
+            )}`
+          )
+        );
+      } else {
+        octokit.log.info(
+          chalk.cyan(
+            ` ℹ No '${actionName}' action found in '${fileName}' which need an update.`
+          )
+        );
+      }
+    } catch (error) {
+      octokit.log.error(
+        chalk.red("There was an error updating the workflow file")
+      );
+      octokit.log.error(error);
+    }
+  }
+}
